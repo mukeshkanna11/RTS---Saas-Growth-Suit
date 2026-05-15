0c80a19b22e68b199747b6d6ea0e1429585618ea
mukeshkanna11 <agriculturistmukesh11@gmail.com>
2026-05-15 13:48:22 +0530

    Team Commit

--- Saas API/src/modules/crm/contacts/contact.controller.js
+++ Saas API/src/modules/crm/contacts/contact.controller.js
@@ -80,3 +80,19 @@ exports.remove = async (req, res) => {
     });
   }
 };
+
+exports.getTeamContacts = async (req, res) => {
+  try {
+    const contacts = [];
+
+    res.json({
+      success: true,
+      data: contacts,
+    });
+  } catch (err) {
+    res.status(500).json({
+      success: false,
+      message: err.message,
+    });
+  }
+};
